b9943be35c085299494ff4580c4154bf1da5267a
noah <ojedanoah24@gmail.com>
2025-11-25 10:58:18 -0800

    Update naive_bayes.py

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,30 +1,51 @@
 #-------------------------------------------------------------------------
-# AUTHOR: your name
-# FILENAME: title of the source file
+# AUTHOR: Noah Ojeda
+# FILENAME: naive_bayes.py
 # SPECIFICATION: description of the program
 # FOR: CS 4440- Assignment #4
-# TIME SPENT: how long it took you to complete the assignment
+# TIME SPENT: 
 #-----------------------------------------------------------*/
 
 #importing some Python libraries
 from sklearn.naive_bayes import GaussianNB
+import numpy as np
+import csv 
+import pandas as pd
 
 #11 classes after discretization
 classes = [i for i in range(-22, 40, 6)]
 
 s_values = [0.1, 0.001, 0.0001, 0.00001, 0.000001, 0.0000001, 0.00000001, 0.000000001, 0.0000000001]
 
+#funciton to discretize the real values to the closest class
+def discretize(value):
+    closest_class = min(classes, key=lambda x: abs(x - value))
+    return closest_class
+
 #reading the training data
 #--> add your Python code here
+filename_training = 'weather_training.csv'
+df = pd.read_csv(filename_training, sep=',', header=0) #elimating the header
+
+X_training = df.iloc[:, :-1].values
+
+Y_training_real = df.iloc[:, -1].values
 
 #update the training class values according to the discretization (11 values only)
 #--> add your Python code here
+Y_training = np.array([discretize(value) for value in Y_training_real])
 
 #reading the test data
 #--> add your Python code here
+filename_test = 'weather_test.csv'
+df_test = pd.read.csv(filename_test, sep=',', header=0) #elimating the header again
 
+X_test = df_test.iloc[:, :-1].values
+
+Y_test_real = df_test.iloc[:, -1].values
 #update the test class values according to the discretization (11 values only)
 #--> add your Python code here
+Y_test = np.array([discretize(value) for value in Y_test_real])
 
 #loop over the hyperparameter value (s)
 #--> add your Python code here
